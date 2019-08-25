61d6efd6e1518d056485741537d9fb7e768bf6ff
Sourav <rakshitsourav3@gmail.com>
2019-08-26 03:20:46 +0530

    added DOM handler/ tweked some css properties

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,10 @@
 //Make connection
 
 const socket  = io.connect('http://localhost:4000');
+
+//query DOM
+
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
