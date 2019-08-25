5009b9b95b316bf3d1dbdec84869413d098c0d48
Sourav <rakshitsourav3@gmail.com>
2019-08-26 03:34:39 +0530

    Chat app working 😀

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const PORT = process.env.PORT | 4000;
 
 app.use(express.static('public'));  //sending index.html from pubic folder
 
-
-
-
-
-
 const server = app.listen(PORT,()=>{
     console.log(`server is running at port ${PORT}`)
 })
@@ -18,6 +13,11 @@ const io = socket(server);
 
 io.on('connection', (socket)=>{
     console.log('Made socket connection', socket.id);
+
+
+    socket.on('chat', (data)=>{
+        io.sockets.emit('chat', data);
+    })
 })
 
 
--- public/chat.js
+++ public/chat.js
@@ -8,3 +8,19 @@ const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+
+
+//Emit events
+
+btn.addEventListener('click', ()=>{
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+})
+
+//Listen for events
+
+socket.on('chat', (data)=>{
+    output.innerHTML+="<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+})
